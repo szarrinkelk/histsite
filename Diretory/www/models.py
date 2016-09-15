88f07d1d006234825f3ad8f31d37e8566971bd6c
szarrinkelk <mobpro@MobPro21-Sepideh.local>
2016-09-15 10:26:47 +0200

    First commit, working app

---
+++ Diretory/www/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length = 40)
+    email = models.CharField(max_length = 100)
+    title = models.CharField(max_length = 100)
+    image = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.name
